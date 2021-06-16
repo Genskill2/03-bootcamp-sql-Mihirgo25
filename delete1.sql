2c7f89f9df3d767d32a7fc22ff0fb86d68c90caa
Gokhale Mihir Madhav <79635177+Mihirgo25@users.noreply.github.com>
2021-06-16 18:47:29 +0530

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -1,5 +1,5 @@
-delete from subjects
+delete name from subjects and subjects.id = 100
 where subjects.name = 'History';
 
 delete from books_subjects
-where books_subjects.subject = NULL;
+where books_subjects.subject = 100;
