b105890ed89d887e6785812575c1b0f476bcddfb
Gokhale Mihir Madhav <79635177+Mihirgo25@users.noreply.github.com>
2021-06-16 14:58:42 +0530

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,7 +1,7 @@
 select subjects.name
 from subjects
 join books_subjects
-on books_subjects.subject = subects.id
+on books_subjects.subject = subjects.id
 join books
 on books.id = books_subjects.book
 where books.title = 'Atomic habits';
