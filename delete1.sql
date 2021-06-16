ab699e7ae6fac348a1d138e0e2a545efafa19d3e
Gokhale Mihir Madhav <79635177+Mihirgo25@users.noreply.github.com>
2021-06-16 14:02:53 +0530

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -1,7 +1,3 @@
 delete
-from books_subjects as bs
-inner join books as b
-on b.id = bs.book
-inner join subjects as s
-on s.id = bs.subject
-where s.name = 'History';
+from subjects as s,books_subjects as bs,books as bs
+where bs.book = books.id and bs.subject = s.id  and s.name = 'History';
